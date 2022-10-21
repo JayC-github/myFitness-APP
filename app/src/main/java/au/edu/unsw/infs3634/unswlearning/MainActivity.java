3d022e9c376f31c818eea04aaed1acc10849e0ce
KaiwenLuo2003 <kwluo2003@gmail.com>
2022-10-21 16:56:23 +1100

    Youtube API Testing not working

--- app/src/main/java/au/edu/unsw/infs3634/unswlearning/MainActivity.java
+++ app/src/main/java/au/edu/unsw/infs3634/unswlearning/MainActivity.java
@@ -1,14 +1,57 @@
 package au.edu.unsw.infs3634.unswlearning;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import com.google.android.youtube.player.YouTubeInitializationResult;
+import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayerView;
 
 public class MainActivity extends AppCompatActivity {
 
+    String api_key = "AIzaSyCvTBA8V3W0z7v8m9aTMj_U0EoEEpKIFEk";
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void
+    onCreate(@Nullable Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Get reference to the view of Video player
+        YouTubePlayerView ytPlayer = (YouTubePlayerView)findViewById(R.id.ytPlayer);
+
+        ytPlayer.initialize(
+                api_key,
+                new YouTubePlayer.OnInitializedListener() {
+                    // Implement two methods by clicking on red
+                    // error bulb inside onInitializationSuccess
+                    // method add the video link or the playlist
+                    // link that you want to play In here we
+                    // also handle the play and pause
+                    // functionality
+                    @Override
+                    public void onInitializationSuccess(
+                            YouTubePlayer.Provider provider,
+                            YouTubePlayer youTubePlayer, boolean b)
+                    {
+                        youTubePlayer.loadVideo("HzeK7g8cD0Y");
+                        youTubePlayer.play();
+                    }
+
+                    // Inside onInitializationFailure
+                    // implement the failure functionality
+                    // Here we will show toast
+                    @Override
+                    public void onInitializationFailure(YouTubePlayer.Provider provider,
+                                                        YouTubeInitializationResult
+                                                                youTubeInitializationResult)
+                    {
+                        Toast.makeText(getApplicationContext(), "Video player Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
     }
 }
+
